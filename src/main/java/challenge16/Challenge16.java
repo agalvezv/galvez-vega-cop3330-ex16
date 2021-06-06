8480db2b1c7380d134499b5e123f65fced3a5c86
agalvezv <43121059+agalvezv@users.noreply.github.com>
2021-06-05 23:13:03 -0400

    Initial commit

---
+++ src/main/java/challenge16/Challenge16.java
@@ -0,0 +1,22 @@
+package challenge16;
+
+import java.util.Scanner;
+
+public class Challenge16 {
+
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+
+        System.out.println("What is your age?");
+        String givenAgeS = inputs.nextLine();
+        int giveAgeI = Integer.parseInt(givenAgeS);
+        String responseOne = "You are not old enough to legally drive.";
+        String responseTwo = "You are old enough to legally drive.";
+
+        String outcome = (giveAgeI >= 16) ? responseTwo : responseOne;
+        System.out.println(outcome);
+
+
+    }
+
+}
